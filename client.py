881dc7ba21e77d4058ded2dbfcf9446be8b96354
EfraimGavito <efraimgavito@gmail.com>
2026-04-09 22:05:39 -0400

    Implement socket connection in client.py
    Added socket connection to server and message sending.

--- client.py
+++ client.py
@@ -1,2 +1,26 @@
-# client.py
+# client.py 
+from socket32 import create_new_socket
+
+HOST = '127.0.0.1'
+PORT = 65431
+
+
+def main():
+
+    print('Hello world')
+
+    # Create socket to connect to the server
+
+    with create_new_socket() as s:
+        s.connect(HOST, PORT)
+
+        s.sendall('richmond grim reaper')
+
+        while True:
+            nettspend = s.recv()
+            print(nettspend)
+            break
+
+if __name__ == '__main__':
+    main()# client.py
 
